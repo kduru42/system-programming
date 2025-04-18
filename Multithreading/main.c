d79dc12ba07345a4106fc84daf85f1d38002332a
kduru42 <k.duru2020@gtu.edu.tr>
2025-04-18 22:35:04 +0300

    all done

--- Multithreading/main.c
+++ Multithreading/main.c
@@ -5,8 +5,9 @@
 #include "string.h"
 #include "semaphore.h"
 #include "time.h"
-#include "queue.h"
 #define ENGINEER_COUNT 3
+#define SATELLITE_LIMIT 10
+#define TIMEOUT 5 // setting timeout
 
 typedef struct {
     int id;
@@ -14,23 +15,24 @@ typedef struct {
 } SatelliteRequest;
 
 pthread_mutex_t engineerMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t countMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t countMutex = PTHREAD_MUTEX_INITIALIZER; // Mutex for handling request count
 int availabeEngineers = ENGINEER_COUNT;
 sem_t newRequest, requestHandled;
 SatelliteRequest *requestQueue;
-int handledRequests = 0;
+int handledRequests = 0; // Number of handled requests
 int maxRequests = 10;
 int queueSize = 0;
 
 void add_request(int id, int priority) {
-    pthread_mutex_lock(&engineerMutex);
+    pthread_mutex_lock(&engineerMutex); // lock to mutex to protect queue datas
     requestQueue[queueSize].id = id;
     requestQueue[queueSize].priority = priority;
     queueSize++;
 
+    // Sort the queue based on priority (highest first)
     for (int i = 0; i < queueSize - 1; i++) {
         for (int j = 0; j < queueSize - i - 1; j++) {
-            if (requestQueue[j].priority > requestQueue[j + 1].priority) {
+            if (requestQueue[j].priority < requestQueue[j + 1].priority) {
                 // Swap requests
                 SatelliteRequest temp = requestQueue[j];
                 requestQueue[j] = requestQueue[j + 1];
@@ -70,48 +72,65 @@ void remove_request(int id) {
 void *engineer(void *arg) {
     int id = *(int *)arg;
     while (1) {
-        sem_wait(&newRequest);
-        pthread_mutex_lock(&engineerMutex);
-        if (queueSize == 0) {
-            pthread_mutex_unlock(&engineerMutex);
-            pthread_mutex_lock(&countMutex);
-            int done = (handledRequests >= maxRequests);
+        // First check if all work is done
+        pthread_mutex_lock(&countMutex);
+        if (handledRequests >= maxRequests - 2) {
             pthread_mutex_unlock(&countMutex);
-            if (done) {
-                printf("[ENGINEER %d] Exiting...\n", id);
-                break;
-            }
+            printf("[ENGINEER %d] Exiting...\n", id);
+            break;
         }
+        pthread_mutex_unlock(&countMutex);
+
+        // Wait for new request
+        sem_wait(&newRequest);
+
+        // Check again in case all work was completed while we were waiting
+        pthread_mutex_lock(&countMutex);
+        if (handledRequests >= maxRequests - 2) {
+            pthread_mutex_unlock(&countMutex);
+            printf("[ENGINEER %d] Exiting...\n", id);
+            break;
+        }
+        pthread_mutex_unlock(&countMutex);
+        pthread_mutex_lock(&engineerMutex);
         availabeEngineers--;
         SatelliteRequest *request = get_request();
+        if (request == NULL) {
+            availabeEngineers++;
+            pthread_mutex_unlock(&engineerMutex);
+            continue; // No request available, continue waiting
+        }
         pthread_mutex_unlock(&engineerMutex);
-        sem_post(&requestHandled);
+
+        sem_post(&requestHandled); // Signal that a request has been handled
         printf("[ENGINEER %d] Handling Satellite %d (Priority %d)\n", id, request->id, request->priority);
         sleep(1); // Simulate work
         pthread_mutex_lock(&engineerMutex);
         availabeEngineers++;
         pthread_mutex_unlock(&engineerMutex);
         printf("[ENGINEER %d] Finished Satellite %d\n", id, request->id);
+        free(request);
         pthread_mutex_lock(&countMutex);
-        handledRequests++;
+        handledRequests++; // Increment handled requests count
         pthread_mutex_unlock(&countMutex);
+        
     }
     return NULL;
 }
 
 void *satellite(void *arg) {
     int id = *(int *)arg;
-    int priority = rand() % 10 + 1; // Random priority between 1 and 10
+    int priority = rand() % SATELLITE_LIMIT + 3; // Random priority between 3 and satellite limit (10 for now)
     printf("[SATELLITE] Satellite %d requesting (Priority %d)\n", id, priority);
     pthread_mutex_lock(&engineerMutex);
     int engineersAvailable = availabeEngineers; // to read safely, avoiding race condition
     pthread_mutex_unlock(&engineerMutex);
     add_request(id, priority);
     sem_post(&newRequest);
-    if (engineersAvailable == 0) {
+    if (engineersAvailable == 0) { // No engineers available, wait for one to finish
         struct timespec timeout;
         clock_gettime(CLOCK_REALTIME, &timeout);
-        timeout.tv_sec += 5; // 5 seconds timeout
+        timeout.tv_sec += TIMEOUT; // TIMEOUT defined as 5 seconds
         if (sem_timedwait(&requestHandled, &timeout) == -1) {
             printf("[TIMEOUT] Satellite %d timedout\n", id);
             pthread_mutex_lock(&engineerMutex);
@@ -130,16 +149,18 @@ void *satellite(void *arg) {
 }
 
 
+
 int main()
 {
     pthread_t engineers[ENGINEER_COUNT];
     pthread_t *satellites;
 
     srand(time(NULL));
-    int num_satellites = rand() % 10 + 1; // Random number of satellites between 1 and 10
+    int num_satellites = rand() % 10 + 3; // Random number of satellites between 1 and 10
     satellites = malloc(num_satellites * sizeof(pthread_t));
     requestQueue = malloc(num_satellites * sizeof(SatelliteRequest));
     maxRequests = num_satellites;
+
     if (satellites == NULL) {
         perror("Failed to allocate memory for satellites");
         return 1;
@@ -172,16 +193,29 @@ int main()
             return 1;
         }
     }
-    for (int i = 0; i < ENGINEER_COUNT; i++) {
-        pthread_join(engineers[i], NULL);
-    }
     for (int i = 0; i < num_satellites; i++) {
         pthread_join(satellites[i], NULL);
     }
-    pthread_mutex_destroy(&engineerMutex);
-    pthread_mutex_destroy(&countMutex);
-    sem_destroy(&newRequest);
-    sem_destroy(&requestHandled);
+    for (int i = 0; i < ENGINEER_COUNT; i++) {
+        pthread_join(engineers[i], NULL);
+    }
+    if (pthread_mutex_destroy(&engineerMutex) != 0) {
+        perror("Failed to destroy mutex");
+        return 1;
+    }
+    if (pthread_mutex_destroy(&countMutex) != 0) {
+        perror("Failed to destroy mutex");
+        return 1;
+    }
+    if (sem_destroy(&newRequest) != 0) {
+        perror("Failed to destroy semaphore");
+        return 1;
+    }
+    if (sem_destroy(&requestHandled) != 0) {
+        perror("Failed to destroy semaphore");
+        return 1;
+    }
     free(satellites);
+    free(requestQueue);
     return 0;
 }
